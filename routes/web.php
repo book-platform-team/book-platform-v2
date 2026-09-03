a17c6704cc336d020e7fa8d24f4f0c5de52448f3
Basmala Boudiaf <basmalaboudiaf04@gmail.com>
2026-09-03 13:50:07 +0200

    إصلاح 404 على الجذر: route صريح يقدم index.html و ملفات public/ مباشرة (php artisan serve لا يقرأ .htaccess)

--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+
+Route::get('/{any}', function () {
+    $path = public_path(request()->path() === '/' ? 'index.html' : request()->path());
+
+    if (file_exists($path) && ! is_dir($path)) {
+        return response()->file($path);
+    }
+
+    return response()->file(public_path('index.html'));
+})->where('any', '^(?!api|admin|storage|sanctum).*$');
