363396665fbcea899d80dd2a8004549af92f4c7a
Basmala Boudiaf <basmalaboudiaf04@gmail.com>
2026-09-05 13:31:43 +0200

    إصلاح seeding على production: استبدال fake() بقيم ثابتة (fakerphp/faker غير مثبتة مع --no-dev)

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -12,11 +12,15 @@ class UserFactory extends Factory
 {
     protected static ?string $password;
 
+    protected static int $counter = 0;
+
     public function definition(): array
     {
+        static::$counter++;
+
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'name' => 'مستخدم '.static::$counter,
+            'email' => 'user'.static::$counter.'@example.com',
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
         ];
